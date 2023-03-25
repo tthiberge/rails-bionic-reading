c395559b1f05b783a09dee46f32bc8a8e625c824
Theophile Thiberge <tthiberge@gmail.com>
2023-03-25 17:41:16 +0100

    Progressing in the stimulus controller

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,14 +3,29 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 export default class extends Controller {
 
-  static targets = ["content"]
+  static targets = ["input", "list", "window"]
 
   connect() {
-    // console.log("Connected")
-    console.log(this.item)
+    console.log("Connected")
+    // console.log(this.element)
   }
 
-  display() {
-    console.log("clicked")
+  display(event) {
+    event.preventDefault()
+    // console.log("clicked")
+    // console.log("submitted") // Marche pas mon data-action sur submit, seulement sur click
+    console.log(this.inputTarget.value)
+
+    // console.log(this.windowTarget)
+    // this.windowTarget.scrollTo(0, this.windowTarget.scrollHeight)
+
+    // console.log(this.listTarget)
+    this.listTarget.insertAdjacentHTML("beforeend", <strong>{this.inputTarget.value}</strong>)
+
+    // Les acolades ont l'ir d'être problématique
+  }
+
+  type() {
+    // console.log(this.inputTarget.value)
   }
 }
