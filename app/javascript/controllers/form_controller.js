82868c3126f8124fca5f57ac879e9a2c165e0840
Theophile Thiberge <tthiberge@gmail.com>
2023-03-26 15:40:31 +0200

    Setting the front basis for the real reading

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,31 +3,40 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 export default class extends Controller {
 
-  static targets = ["input", "list", "window"]
+  static targets = ["input", "list", "window", "result"]
 
   connect() {
     console.log("Connected")
-    // console.log(this.element)
   }
 
   display(event) {
     event.preventDefault()
     // console.log("clicked")
-    // console.log("submitted") // Marche pas mon data-action sur submit, seulement sur click. Submit se met sur le formulaire, pas le bouton!! ça marche!
+    // console.log("submitted") // Submit se met sur le formulaire, pas le bouton!! ça marche!
     console.log(this.inputTarget.value)
 
-    // console.log(this.windowTarget)
-    // this.windowTarget.scrollTo(0, this.windowTarget.scrollHeight)
+    // Construire le séquençage dont j'ai besoin pour savoir ce que je mets en bold
+    const boldResult = `<strong>${this.inputTarget.value}</strong>`
 
-    console.log(this.listTarget)
-    const contentValue  = `<li><strong>${this.inputTarget.value}</strong></li>`
-    console.log(contentValue)
-    this.listTarget.insertAdjacentHTML("beforeend", contentValue)
+    this.resultTarget.insertAdjacentHTML("afterbegin", boldResult)
 
-    // Les acolades ont l'ir d'être problématique
+
+    // this.resultTarget.insertAdjacentHTML("afterbegin", `<strong>${this.inputTarget.value}</strong>`)
+    // this.listTarget.insertAdjacentHTML("afterbegin", `<li><strong>${this.inputTarget.value}</strong></li>`)
+
+
+
+  }
+
+  reloadPage() {
+    window.location.reload()
   }
 
   type() {
     // console.log(this.inputTarget.value)
   }
 }
+
+// Scroll en bas de page à faire fonctionner ensuite
+    // console.log(this.windowTarget)
+    // this.windowTarget.scrollTo(0, this.windowTarget.scrollHeight)
